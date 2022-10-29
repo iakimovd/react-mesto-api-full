07225064e2fc8f0daab746caa6258be4a8c33be8
Dmitrii <dimitry.vs@gmail.com>
2022-10-29 15:15:26 +0500

    try to fix CORS

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { PORT = 4000 } = process.env;
 const app = express();
 
 app.use(cors({
-  origin: ['http://iakimovd.project.mesto.nomoredomains.icu/', 'https://iakimovd.project.mesto.nomoredomains.icu/'],
+  origin: ['https://iakimovd.project.mesto.nomoredomains.icu', 'http://iakimovd.project.mesto.nomoredomains.icu'],
   credentials: true,
 }));
 
